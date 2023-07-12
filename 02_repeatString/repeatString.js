93b18570c2134d0dd9441c45c5f1c7b29df2231f
pour-moi <pour64moi@gmail.com>
2023-07-12 21:39:37 +0300

    (Solved) Repeat_String

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, repeat) {
+  if (repeat < 0) {
+    return "ERROR";
+  }
+  let Word = "";
+  for (let i = 0; i < repeat; i++) {
+    Word += word;
+  }
+  return Word;
 };
 
 // Do not edit below this line
