9b91098841a571a234739c9d295cc87cd8d0ac3c
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-17 20:31:19 +0000

    leap year using if condition

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+     if(year % 4 === 0){
+        if(year % 100 !== 0 || year % 400 === 0){
+            console.log("It is a leap year");
+        }
+     } 
 };
 
 // Do not edit below this line
